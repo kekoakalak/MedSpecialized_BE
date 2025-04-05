d49da4459974dc7c27024201b0c26d8cfebd8860
kekoakalak <mackenzieborneo@gmail.com>
2025-04-05 20:36:08 +0800

    initialize db configuration

---
+++ MedSpecialized_BE/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+
+            connectTimeoutMS: 10000,
+            socketTimeoutMS: 45000,
+        });
+        console.log('MongoDB connected successfully');
+
+    }catch(error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;

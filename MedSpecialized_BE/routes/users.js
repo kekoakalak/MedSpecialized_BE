fb665ea23922df5c57d06a4bc386fa1803d3b304
kekoakalak <mackenzieborneo@gmail.com>
2025-04-06 17:03:38 +0800

    Initialized Post http request in user

--- MedSpecialized_BE/routes/users.js
+++ MedSpecialized_BE/routes/users.js
@@ -18,3 +18,37 @@ router.get('/', auth, adminOnly, async (req, res) => {
 
 });
 
+
+//Create a new user (admin only)
+router.post('/', auth, adminOnly, async (req, res) => {
+
+    //Set user credential
+    const { email, password, name, role, status } = req.body;
+    if (!validator.isEmail){
+       return res.status(400).json({message: 'Invalid Email Address'});
+    } 
+    //Check if user already exist
+    try{
+        let user = await User.findOne({email});
+        if (user){
+            return res.status(400).json({message: 'User already exist'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = new User({
+            email,
+            hashedPassword,
+            name,
+            role: role || 'Trainee',
+            status: status || 'Active',
+
+        });
+
+        await user.save();
+        res.status(201).json({message: 'User created successfully'}); //OK
+
+    }catch(error) {
+        res.status(500).json({message: 'Server Error'});
+    } 
+
+});

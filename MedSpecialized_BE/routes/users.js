515dd61bf0f08dfdc4e31b3ff5242e77277ad4e0
kekoakalak <mackenzieborneo@gmail.com>
2025-04-06 16:42:53 +0800

    Initialize Get http request in REST

--- MedSpecialized_BE/routes/users.js
+++ MedSpecialized_BE/routes/users.js
@@ -5,3 +5,16 @@ const validator = require('validator');
 const User = require ('../models/User');
 const { auth, adminOnly } = require('../middleware/auth');
 
+
+//Get all users (admin only)
+router.get('/', auth, adminOnly, async (req, res) => {
+    try{
+        const users = await User.find().select('-password');
+        res.json(users);
+
+    }catch (error) {
+        res.status(500).json({message: 'Server error'});
+    }
+
+});
+
